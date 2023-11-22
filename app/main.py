282bb63084d25c053820a75d528b98ca0a51906e
PeretEtienne <etienne.peret@integragen.com>
2023-11-22 14:16:32 +0000

    devcontainer with docker-compose

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
+import pprint
 from typing import TypedDict
 
+import vcf
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -12,6 +14,46 @@ class HomeResponse(TypedDict):
 @app.get("/")
 def read_root() -> HomeResponse:
     test = "hey"
+    print(test)
     uppercased = test.upper()
 
     return {"Hello": uppercased}
+
+
+@app.get("/vcf")
+def read_vcf():
+    with open("KDM39442_59040_492838_467052_base_sort.vcf") as f:
+        vcf_reader = vcf.Reader(f)
+
+        variants = list(vcf_reader)
+
+        pp = pprint.PrettyPrinter(indent=4)
+        pp.pprint(vars(variants[0]))
+
+        pp.pprint(vars(variants[5]))
+
+        # print("first variant:", variants[0].samples)
+        # print("11 variant", variants[10].samples)
+
+        # variant = variants[5]
+        # print("CHROM", variant.CHROM)
+        # print("POS", variant.POS)
+        # print("ID", variant.ID)
+        # print("REF", variant.REF)
+        # print("ALT", variant.ALT)
+        # print("QUAL", variant.QUAL)
+        # print("FILTER", variant.FILTER)
+        # print("INFO", variant.INFO)
+        # print("FORMAT", variant.FORMAT)
+        # print("samples", variant.samples)
+        # print("Gene : ", variant.INFO["GENE"])
+
+        # all_types = [v.INFO["TYPE"] for v in variants]
+        # unique_types = set(all_types)
+
+        # print("all_types", unique_types)
+
+        # print("frequency", variants[0].samples[0].data.VF)
+        # print(len(variants))
+
+        return "test"
