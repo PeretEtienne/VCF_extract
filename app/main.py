4d3e7537fc3a1927ad336e83722b8b0cdf1972d3
PeretEtienne <etienne.peret@integragen.com>
2023-11-23 15:24:58 +0000

    open file coming from request

--- app/main.py
+++ app/main.py
@@ -1,14 +1,17 @@
-import pprint
-import time
+import io
+import logging
 
 from elasticsearch import AsyncElasticsearch, helpers
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 import vcf
 
 app = FastAPI()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class Settings(BaseSettings):
@@ -30,33 +33,26 @@ def get_es():
     )
 
 
-@app.get(path="/")
-async def read_root():
-    with open("vcf/KDM39442_59040_492838_467052_base_sort.vcf") as f:
-        vcf_reader = vcf.Reader(f)
-        variations = list(vcf_reader)
+@app.post(path="/extract")
+async def extract_variations(file: UploadFile):
+    file_content = await file.read()
+    vcf_reader = vcf.Reader(io.TextIOWrapper(io.BytesIO(file_content)))
+    variations = list(vcf_reader)
 
-        es = get_es()
+    es = get_es()
 
-        start = time.time()
-        documents = [
-            {
-                "chrom": v.CHROM,
-                "pos": v.POS,
-                "id": v.ID,
-            }
-            for v in variations
-        ]
-        end_list = time.time() - start
+    documents = [
+        {
+            "chrom": v.CHROM,
+            "pos": v.POS,
+            "id": v.ID,
+        }
+        for v in variations
+    ]
 
-        print("end_list", end_list)
+    result = await helpers.async_bulk(es, documents, index="variations")
 
-        start = time.time()
-        result = await helpers.async_bulk(es, documents, index="variations")
-        print("end_bulk", time.time() - start)
-
-        print(result)
-        return result
+    return result
 
 
 @app.get("/search")
@@ -67,48 +63,9 @@ async def get_variations():
     return result
 
 
-@app.get("/delete")
+@app.delete(path="/variations")
 async def delete_variations():
     es = get_es()
 
     result = await es.delete_by_query(index="variations", query={"match_all": {}})
     return result
-
-
-@app.get("/vcf")
-def read_vcf():
-    with open("vcf/KDM39442_59040_492838_467052_base_sort.vcf") as f:
-        vcf_reader = vcf.Reader(f)
-
-        variants = list(vcf_reader)
-
-        pp = pprint.PrettyPrinter(indent=4)
-        pp.pprint(vars(variants[0]))
-
-        pp.pprint(vars(variants[5]))
-
-        # print("first variant:", variants[0].samples)
-        # print("11 variant", variants[10].samples)
-
-        # variant = variants[5]
-        # print("CHROM", variant.CHROM)
-        # print("POS", variant.POS)
-        # print("ID", variant.ID)
-        # print("REF", variant.REF)
-        # print("ALT", variant.ALT)
-        # print("QUAL", variant.QUAL)
-        # print("FILTER", variant.FILTER)
-        # print("INFO", variant.INFO)
-        # print("FORMAT", variant.FORMAT)
-        # print("samples", variant.samples)
-        # print("Gene : ", variant.INFO["GENE"])
-
-        # all_types = [v.INFO["TYPE"] for v in variants]
-        # unique_types = set(all_types)
-
-        # print("all_types", unique_types)
-
-        # print("frequency", variants[0].samples[0].data.VF)
-        # print(len(variants))
-
-        return "test"
